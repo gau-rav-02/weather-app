c155be8ade8670c257c90c02c45b2f6d25dedf87
gau-rav-02 <gauravsl2feb@gmail.com>
2025-09-29 13:30:19 +0530

    first commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const BaseURL = 'https://api.weatherapi.com/v1/current.json?key=6bb7168600a74ce08aa73856252909&q='
+const BaseURL = 'https://api.weatherapi.com/v1/current.json?key=API-KEY&q='
 
 export const getWeatherData = async (city) => {
     const response = await fetch(`${BaseURL}${city}&aqi=yes`);
